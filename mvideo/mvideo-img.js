6b88fd6eb44ec9a97f706eb840aa9e4c9b29bf8a
PavelSigeti <sigeti352@gmail.com>
2024-02-20 16:42:51 +0300

    Mvideo upd

--- mvideo/mvideo-img.js
+++ mvideo/mvideo-img.js
@@ -2,15 +2,15 @@ import * as cheerio from 'cheerio';
 import puppeteer from 'puppeteer';
 
 export const mvideoImg = async (link) => {
-    const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome', args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
 
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0');
     await page.setViewport({ width: 1920, height: 1080 });
 
     page.setDefaultNavigationTimeout(0);
-    
+
     await page.goto(link, { waitUntil: 'networkidle0' });
 
     const selector = '.product-carousel__preview'; 
