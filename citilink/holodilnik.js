bd5d7dc77f65e47b94dd151d332603c1754670e6
PavelSigeti <sigeti352@gmail.com>
2024-02-26 17:21:21 +0300

    Holod + stiralka

--- citilink/holodilnik.js
+++ citilink/holodilnik.js
@@ -1,3 +1,5 @@
+import { translit } from "../translit.js";
+
 const mainParser = (val, num) => (num) ? +val.replace(/[^.\d]/g, '') : val;
 
 const data = {
@@ -20,7 +22,6 @@ const data = {
         'Температура морозильной камеры min': ['minimalnaia-temperatura-v-morozilnoi-kamere', 'Минимальная температура в морозильной камере'],
         'Материал полок': ['material-polok', 'Материал полок'],
         'Количество полок': ['kolichestvo-polok', 'Количество полок'],
-
     }
 };
 
@@ -56,6 +57,7 @@ export const holodilnik = async (attrData) => {
             };
             console.log(item);
         }
+        if(attrData[item]) delete attrData[item];
     });
 
     const svg = attrData['Размеры (ШхВхГ)'].split(' х ').map(val=>mainParser(val, true));
@@ -63,13 +65,13 @@ export const holodilnik = async (attrData) => {
     attr['vysota'] = {name: 'Высота', value: svg[1]};
     attr['glubina'] = {name: 'Глубина', value: svg[2]};
 
-    Object.keys(data.extra).forEach((item) => {
-        if(attrData[item]) {
-            extraAttr[data.extra[item][0]] = {
-                name: data.extra[item][1],
-                value: attrData[item],
-              };
-        }
+    if(attrData['Размеры (ШхВхГ)']) delete(attrData['Размеры (ШхВхГ)']);
+
+    Object.keys(attrData).forEach((item) => {
+        extraAttr[translit(item)] = {
+            name: item,
+            value: attrData[item],
+        };
     });
 
     return {"result": true, attr: attr, extra: extraAttr};
